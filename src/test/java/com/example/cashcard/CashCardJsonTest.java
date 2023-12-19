ab89d6268ab427d9aed402468797d1941263e9e7
5OO <git.hub@myrk.eu>
2023-12-19 20:48:27 +0200

    second test to deserialize data so that it converts from JSON to Java

--- src/test/java/com/example/cashcard/CashCardJsonTest.java
+++ src/test/java/com/example/cashcard/CashCardJsonTest.java
@@ -23,5 +23,18 @@ class CashCardJsonTest {
         assertThat(json.write(cashCard)).extractingJsonPathNumberValue("@.id").isEqualTo(99);
         assertThat(json.write(cashCard)).hasJsonPathNumberValue("@.amount");
         assertThat(json.write(cashCard)).extractingJsonPathNumberValue("@.amount").isEqualTo(123.45);
-                    }
+    }
+
+    @Test
+    void cashCardDeserialisationTest() throws IOException {
+        String expected = """
+                {
+                "id":99,
+                "amount":123.45
+                }
+                """;
+        assertThat(json.parse(expected)).isEqualTo(new CashCard(1000L, 67.89));
+        assertThat(json.parseObject(expected).id()).isEqualTo(1000);
+        assertThat(json.parseObject(expected).amount()).isEqualTo(67.89);
+    }
 }
