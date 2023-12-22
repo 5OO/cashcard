c269217b504683ad2d067ebb59c855f22fe86d89
5OO <git.hub@myrk.eu>
2023-12-22 10:03:19 +0200

    configured to listen for and handle HTTP requests to /cashcards endpoint

--- src/main/java/com/example/cashcard/CashCardController.java
+++ src/main/java/com/example/cashcard/CashCardController.java
@@ -1,8 +1,15 @@
 package com.example.cashcard;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-public class CashCardController {
+@RestController
+@RequestMapping("/cashcards")
+class CashCardController {
+
+    @GetMapping("/{requestedId}")
     private ResponseEntity<String> findById() {
         return ResponseEntity.ok("{}");
     }
