2f9ca45d1d7b0f44d613ff3c1f8ee06e65f193ab
5OO <git.hub@myrk.eu>
2023-12-23 11:33:33 +0200

    Added a serialization test for the Cash Card list.

--- src/test/java/com/example/cashcard/CashCardJsonTest.java
+++ src/test/java/com/example/cashcard/CashCardJsonTest.java
@@ -54,4 +54,9 @@ class CashCardJsonTest {
         assertThat(json.parseObject(expected).id()).isEqualTo(99L);
         assertThat(json.parseObject(expected).amount()).isEqualTo(123.45);
     }
+
+    @Test
+    void cashCardListSerializationTest() throws IOException {
+        assertThat(jsonList.write(cashCards)).isStrictlyEqualToJson("list.json");
+    }
 }
