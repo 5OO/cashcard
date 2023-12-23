336fe18873090a95e5238673cd61e26fa33b91f8
5OO <git.hub@myrk.eu>
2023-12-23 11:20:50 +0200

    refactored CashCardJsonTest.java

--- src/test/java/com/example/cashcard/CashCardJsonTest.java
+++ src/test/java/com/example/cashcard/CashCardJsonTest.java
@@ -1,11 +1,14 @@
 package com.example.cashcard;
 
+import org.assertj.core.util.Arrays;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.boot.test.json.JacksonTester;
 
 import java.io.IOException;
+//import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -14,6 +17,20 @@ class CashCardJsonTest {
 
     @Autowired
     private JacksonTester<CashCard> json;
+
+    @Autowired
+    private JacksonTester<CashCard[]> jsonList;
+
+    private CashCard[] cashCards;
+
+    @BeforeEach
+    void setUp() {
+        cashCards = Arrays.array(
+                new CashCard(99L, 123.45),
+                new CashCard(100L, 100.00),
+                new CashCard(101L, 150.00)
+        );
+    }
 
     @Test
     void cashCardSerializationTest() throws IOException {
