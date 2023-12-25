7929ce9727886622da65f37318f8ca8c14f1a52b
5OO <git.hub@myrk.eu>
2023-12-25 22:00:17 +0200

    The @Configuration annotation tells Spring to use this class to configure Spring and Spring Boot itself. Any Beans specified in this class will now be available to Spring's Auto Configuration engine.

--- src/main/java/com/example/cashcard/SecurityConfig.java
+++ src/main/java/com/example/cashcard/SecurityConfig.java
@@ -1,11 +1,13 @@
 package com.example.cashcard;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 public class SecurityConfig {
     SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity.build();
