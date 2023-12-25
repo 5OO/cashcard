f379c7c1259c0e8055946459a0f13b4e5497187e
5OO <git.hub@myrk.eu>
2023-12-25 22:26:54 +0200

    All HTTP requests to cashcards/ endpoints are required to be authenticated using HTTP Basic Authentication security (username and password).
    Also, do not require CSRF security.

--- src/main/java/com/example/cashcard/SecurityConfig.java
+++ src/main/java/com/example/cashcard/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.cashcard;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -9,7 +10,13 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+    @Bean
     SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(request -> request
+                        .requestMatchers("/cashcards/**")
+                        .authenticated())
+                .csrf(csrf -> csrf.disable())
+                .httpBasic(Customizer.withDefaults());
         return httpSecurity.build();
     }
 
