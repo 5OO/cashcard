f3c8d925f489d225e42c4efa0a8521d9c3348edb
5OO <git.hub@myrk.eu>
2023-12-26 22:09:55 +0200

    Implemented the DELETE Endpoint -> added code to the Controller to delete the record.

--- src/main/java/com/example/cashcard/CashCardController.java
+++ src/main/java/com/example/cashcard/CashCardController.java
@@ -76,6 +76,7 @@ class CashCardController {
 
     @DeleteMapping("/{id}")
     private ResponseEntity<Void> deleteCashCard(@PathVariable Long id) {
+        cashCardRepository.deleteById(id);
         return ResponseEntity.noContent().build();
     }
 }
