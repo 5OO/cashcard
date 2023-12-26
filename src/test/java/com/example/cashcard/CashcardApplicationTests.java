e894248c2111500b670a81d37d388043eec8100c
5OO <git.hub@myrk.eu>
2023-12-26 21:59:04 +0200

    implemented the Delete endpoint along with the 204 NO CONTENT status code.

--- src/test/java/com/example/cashcard/CashcardApplicationTests.java
+++ src/test/java/com/example/cashcard/CashcardApplicationTests.java
@@ -193,4 +193,13 @@ class CashcardApplicationTests {
                 .exchange("/cashcards/102", HttpMethod.PUT, request, Void.class);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
     }
+
+    @Test
+    @DirtiesContext
+    void shouldDeleteAnExistingCashCard() {
+        ResponseEntity<Void> response = restTemplate
+                .withBasicAuth("sarah1", "abc123")
+                .exchange("/cashcards/99", HttpMethod.DELETE, null, Void.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
+    }
 }
